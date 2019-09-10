f1b68a71b782eb851c35ea2ea913df6ef66268fc
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-09-09 20:41:26 -0700

    fixed bug with non appearing gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,18 +6,20 @@ for (var i = 0; i < topics.length; i++) {
 
 $(".button").on('click', function () {
     var q = $(this).text();
+    console.log(q)
     var queryURL = `https://api.giphy.com/v1/gifs/search?q=${q}&limit=10&api_key=7diNMvkB3SH15EgXNqytuN3Tyg5lGTDp`;
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
+        console.log(response)
         appendResult(response);
     });
 });
 
 const appendResult = function(r) {
     for (let i = 0; i < r.data.length; i++) {
-        $("#animals").append(`<img src=${r.data[i].bitly_url} >`)
+        $("#animals").append(`<img src=${r.data[i].images["480w_still"].url} >`)
     }
 }
 
