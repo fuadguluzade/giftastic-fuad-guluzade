0bdd499c69271a3c8032bef5c336e13161186b96
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-09-10 20:15:54 -0700

    basic part of assigment done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,52 @@
-topics = ["dog", "cat", "parrot"];
+var topics = ["dog", "cat", "parrot", "hamster"];
+var animatedGIF = [];
+var staticImgs = [];
+var ratings = [];
 
 for (var i = 0; i < topics.length; i++) {
     $("#buttons").append($('<button/>').text(topics[i]).addClass("button"));
 }
 
-$(".button").on('click', function () {
+$("#buttons").on('click', '.button', function () {
     var q = $(this).text();
-    console.log(q)
     var queryURL = `https://api.giphy.com/v1/gifs/search?q=${q}&limit=10&api_key=7diNMvkB3SH15EgXNqytuN3Tyg5lGTDp`;
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        console.log(response)
-        appendResult(response);
+        console.log(response);
+        for (var i = 0; i < response.data.length; i++) {
+            animatedGIF[i] = response.data[i].images.fixed_height.url;
+            ratings[i] = response.data[i].rating;
+            staticImgs[i] = response.data[i].images["480w_still"].url;
+        }
+        appendResult();
     });
 });
 
-const appendResult = function(r) {
-    for (let i = 0; i < r.data.length; i++) {
-        $("#animals").append(`<img src=${r.data[i].images["480w_still"].url} >`)
+$("#animals").on('click', ".gif", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr('src', animatedGIF[$(this).index() / 2]);
+        $(this).attr("data-state", "animated");
+    } else {
+        $(this).attr('src', staticImgs[$(this).index() / 2]);
+        $(this).attr("data-state", "still");
+    }
+    
+
+});
+
+$("#animal-button").on('click', function () {
+    event.preventDefault();
+    $("#buttons").append($('<button/>').text($("#animal-input").val()).addClass("button"));
+});
+
+const appendResult = function () {
+    $("#animals").empty();
+    for (let i = 0; i < staticImgs.length; i++) {
+        $("#animals").append(`<img class="gif" src=${staticImgs[i]} data-state = still>`);
+        $("#animals").append(`<p>Rating: ${ratings[i]}</p>`);
     }
 }
 
